5bf98272aa2ae486b1ef8e874a38e20f43f689bf
Alejandro Urrutia <colorale@Alejandros-iMac-2.local>
2016-04-06 22:18:29 +0800

    Theme Support
    Reorder and cleanup

--- functions.php
+++ functions.php
@@ -29,81 +29,80 @@ Add theme supports
  **********************/
 
 function yanse_theme_support() {	// Change names for your convinience
-/**
- *	Post Thumbnails
- * 	@since	Wordpress 3.0
- *	@since	yanse 0.1
- *	Source:	Wordpress Yanse Theme ( http://bit.ly/AwCLm5 | http://bit.ly/y4Jj2V )
- *
- */
-    add_theme_support('post-thumbnails');
-    // set_post_thumbnail_size(150, 150, false);
-    add_image_size( 'thumb-large', 1024, 99999);
-    add_image_size( 'thumb-medium', 768, 99999);
-    add_image_size( 'thumb-small', 320, 9999);
 
+  /**
+   *	WordPress Theme support
+   *	@since	yanse 1
+   *
+   */
+  add_theme_support( 'title-tag' );
+  // content width
+  if ( ! isset( $content_width ) ) $content_width = 1440;
+  // Feeds
+  add_theme_support('automatic-feed-links');
+  // Add HTML5 Support
+  add_theme_support( 'html5',
+           array(
+            'comment-list',
+            'comment-form',
+            'search-form',
+           )
+  );
 
-/**
- *	Post Formats
- * 	@since	Wordpress 3.1
- *	@since	yanse 0.1
- *	Source:	Wordpress ( http://goo.gl/cSmTX )
- *
- */
- 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video' , 'audio', 'chat' ) );
-
-/**
- *	Sidebars and Widgets
- * 	@since	Wordpress 3.1
- *	@since	yanse 0.1
- *
- */
-	// create widget areas: sidebar, footer
-	$sidebars = array('Sidebar');
-	foreach ($sidebars as $sidebar) {
-	    register_sidebar(array('name'=> $sidebar,
-	    	'id' => 'Sidebar',
-	        'before_widget' => '<article id="%1$s" class="panel widget %2$s">',
-	        'after_widget' => '</article>',
-	        'before_title' => '<h4>',
-	        'after_title' => '</h4>'
-	    ));
-	}
-	$sidebars = array('Footer');
-	foreach ($sidebars as $sidebar) {
-	    register_sidebar(array('name'=> $sidebar,
-	    	'id' => 'Footer',
-	        'before_widget' => '<div class="large-3 columns"><article id="%1$s" class="panel widget %2$s">',
-	        'after_widget' => '</article></div>',
-	        'before_title' => '<h4>',
-	        'after_title' => '</h4>'
-	    ));
-	}
-
-
-/**
- *	Localization
- *	@since	yanse 1
- *	Source:	ayudawordpress | Smashing Magazine ( http://bit.ly/Rglxkj | http://bit.ly/RglzIK )
- *
- */
-
+  // Localization
     load_theme_textdomain('yanse', get_template_directory() . '/languages');
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-	/* <?php _e( 'zzz', 'yanse' ); ?> */
-	/* <?php __( 'xxx', 'yanse' ); ?> */
+  	$locale = get_locale();
+  	$locale_file = get_template_directory() . "/languages/$locale.php";
+  	if ( is_readable( $locale_file ) )
+  		require_once( $locale_file );
+  	/* <?php _e( 'zzz', 'yanse' ); ?> */
+  	/* <?php __( 'xxx', 'yanse' ); ?> */
+  }
 
-    // rss
-    //add_theme_support('automatic-feed-links');
+  // Post Thumbnails
 
+  add_theme_support('post-thumbnails');
+  // set_post_thumbnail_size(150, 150, false);
+  add_image_size( 'thumb-large', 1024, 99999);
+  add_image_size( 'thumb-medium', 768, 99999);
+  add_image_size( 'thumb-small', 320, 9999);
+
+
+  //	Post Formats
+  add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video' , 'audio', 'chat' ) );
+
+
+
+
+
+add_action('after_setup_theme', 'yanse_theme_support');
+
+/* end theme support */
+// Sidebars and Widgets
+function yanse_widgets_init() {
+  $sidebars = array('Sidebar');
+  foreach ($sidebars as $sidebar) {
+      register_sidebar(array('name'=> $sidebar,
+        'id' => 'Sidebar',
+          'before_widget' => '<article id="%1$s" class="panel widget %2$s">',
+          'after_widget' => '</article>',
+          'before_title' => '<h4>',
+          'after_title' => '</h4>'
+      ));
+  }
+
+  $sidebars = array('Footer');
+  foreach ($sidebars as $sidebar) {
+      register_sidebar(array('name'=> $sidebar,
+        'id' => 'Footer',
+          'before_widget' => '<div class="large-3 columns"><article id="%1$s" class="panel widget %2$s">',
+          'after_widget' => '</article></div>',
+          'before_title' => '<h4>',
+          'after_title' => '</h4>'
+      ));
+  }
 }
-
-
-add_action('after_setup_theme', 'yanse_theme_support'); /* end theme support */
-
+add_action( 'widgets_init', 'yanse_widgets_init' );
 
 
 // return entry meta information for posts, used by multiple loops.
