05b375cd3aa46197b9be78dd688273c7002a29c1
Alejandro Urrutia <colorale@Alejandros-iMac-2.local>
2016-04-16 16:46:47 +0800

    deregister fix

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -2,10 +2,9 @@
 /*********************
 Enqueue the proper JS
 *********************/
-function yanse_enqueue_scripts()
-{
-    if ( !is_admin() )
-    {
+if ( ! is_admin() ) {
+    add_action( 'wp_enqueue_scripts', function(){
+
 		wp_deregister_script('jquery');
 		wp_register_script('jquery', get_stylesheet_directory_uri() . '/js/jquery-min.js', true, '2.1.4', false);
 		wp_enqueue_script('jquery');
@@ -14,8 +13,10 @@ function yanse_enqueue_scripts()
 		wp_enqueue_script('foundation', get_stylesheet_directory_uri() . '/js/foundation.js', array('jquery'), '6.1.2', true);
 		// app Foundation
 		wp_enqueue_script('app', get_stylesheet_directory_uri() . '/js/app.js', array('jquery'), '1', true);
-		}
+    });
 }
-add_action( 'wp_print_scripts', 'yanse_enqueue_scripts');
 
+if ( !is_admin() )
+{
+}
 ?>
